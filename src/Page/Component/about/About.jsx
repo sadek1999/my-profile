91c097db0e7658e0d7965b9577a5a8db98041887
Sadek Ali <sadekali1530@gmail.com>
2023-12-18 15:06:54 +0600

    set about section

--- src/Page/Component/about/About.jsx
+++ src/Page/Component/about/About.jsx
@@ -1,9 +1,29 @@
 
-
+import profileimg from './../../../img/Firefly_20231209154000-removebg-preview.png'
 const About = () => {
     return (
-        <div>
-            <h1>This is about page -----------------------------</h1>
+        <div id='about'>
+            <h1 className="text-center text-4xl font-bold mb-10">About Me </h1>
+            <div className=' flex flex-col-reverse  md:flex-row justify-center items-center gap-5 md:gap-20 '>
+                <div className=' flex items-center justify-center'>
+                    <img className='h-60 w-64' src={profileimg} alt="" />
+                </div>
+                <div className='w-[400px]'>
+                    <p className=''>
+                        Hello! I'm Nur Mohammad Sadek Ali, a MERN stack developer based in Dhaka, Bangladesh.
+                        <br />
+                        <br />
+                        Currently pursuing a degree in Computer Science and Engineering from
+                        <span className="text-blue-300 font-semibold"> Eastern University</span>. As a MERN
+                        Stack Developer, I specialize in Node.js,Express.js , and MongoDB for
+                        robust data management. On the front-end, I leverage React, 
+                        Tailwind CSS  to create captivating and responsive web
+                        applications, delivering exceptional user experiences. I excel in
+                        collaborative settings, working seamlessly with cross-functional teams
+                        to deliver exceptional web applications.
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Page/Component/banner/Banner.jsx
+++ src/Page/Component/banner/Banner.jsx
@@ -2,17 +2,17 @@
 
 const Banner = () => {
     return (
-        <div id="home" className='flex min-h-screen justify-center items-center '>
-            <div className="space-y-2 w-1/2 ">
-                <h1 className="text-4xl font-bold">Nur Mohammad Sadek Ali</h1>
+        <div id="home" className='flex flex-col md:flex-row min-h-screen justify-center items-center  '>
+            <div className="space-y-2 w-full lg:w-1/2 lg:pl-20 ">
+                <h1 className=" text-2xl lg:text-4xl font-bold">Nur Mohammad Sadek Ali</h1>
                 <p className="text-xl"> ---MERN stack developer</p>
                 <p className=" w-96 pt-8 pb-6 text-xl">I am a MERN Stack developer base in Dhaka,Bangladesh ,with proficiency in a wide range of modern web technologies</p>
                 <button className="btn btn-outline btn-info ">Download resume</button>
 
             </div>
-            <div className="w-1/2">
+            {/* <div className=" w-full lg:w-1/2 ">
                 <img src='https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/194752812/original/7802a9355c3d1ebe05c0e0cfe28fad3f9aac9a40/develop-full-stack-web-applications-using-mern-stack.jpeg' alt="" />
-            </div>
+            </div> */}
         </div>
     );
 };
